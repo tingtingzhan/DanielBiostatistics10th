2290f233f0755db1089071463320c4042e1233ea
Tingting Zhan <tingtingzhan@gmail.com>
2024-08-29 09:31:39 -0400

    test

--- R/Gosset_Welch.R
+++ R/Gosset_Welch.R
@@ -1,13 +1,10 @@
 
-# Do NOT edit DanielBiostatistics10th/R/Gosset_Welch.R  
-# Edit tzh/R/Gosset_Welch.R
-
 # https://en.wikipedia.org/wiki/Student%27s_t-test
 # https://en.wikipedia.org/wiki/Welch%27s_t-test
 # https://en.wikipedia.org/wiki/Welch–Satterthwaite_equation
 
 
-#' @title Two-Sample Student's \eqn{t}-statistic and Welch–Satterthwaite Equation
+#' @title Degree of Freedom and Standard Error of Two-Sample \eqn{t}-statistic, by Student and Welch–Satterthwaite Equation
 #' 
 #' @description
 #' To determine the degree of freedom, as well as the standard error,
@@ -21,19 +18,33 @@
 #' Default \eqn{v_1=s_1^2}, \eqn{v_0=s_0^2}.
 #' 
 #' @param n1,n0 \link[base]{integer} scalars or \link[base]{vector}s, 
-#' sample sizes of the treatment and control sample, respectively
+#' sample sizes \eqn{n_1} and \eqn{n_0} of the treatment and control sample, respectively
 #' 
 #' @param var.equal \link[base]{logical} scalar, 
-#' whether to treat the two variances \eqn{v_1} and \eqn{v_0} as being equal 
-#' when calculating the degree of freedom and the standard error of the mean-difference.
-#' If `FALSE` (default), Welch–Satterthwaite equation is used.
-#' If `TRUE`, the original two-sample \eqn{t}-test from William Sealy Gosset is used.
-#' See \link[stats]{t.test.default}.
+#' whether to assume \eqn{v_1=v_0}, default `FALSE`,
+#' see \link[stats]{t.test.default}.
+#' 
+#' @details
+#' If \eqn{v_1=v_0} is assumed, the two-sample \eqn{t}-statistic 
+#' from William Sealy Gosset (a.k.a., Student) has 
+#' standard error \eqn{s_{\bar\Delta}=s_p\sqrt{1/n_1+1/n_0}}
+#' with degree-of-freedom \eqn{n_1+n_0-2},
+#' where \eqn{s_p} is the pooled standard deviation,
+#' \deqn{s^2_p=\dfrac{(n_1-1)v_1+(n_0-1)v_0}{n_1+n_0-2}}
+#' 
+#' If \eqn{v_1\neq v_0}, the two-sample \eqn{t}-statistic has 
+#' standard error \eqn{s_{\bar\Delta}=\sqrt{v_1/n_1 + v_0/n_0}} with 
+#' degree of freedom based on Welch–Satterthwaite equation,
+#' \deqn{\dfrac{\left(\dfrac{v_1}{n_1}+\dfrac{v_0}{n_0}\right)^2}{\dfrac{(v_1/n_1)^2}{n_1-1}+\dfrac{(v_0/n_0)^2}{n_0-1}}}
 #' 
 #' @returns 
 #' 
-#' Function [Gosset_Welch] returns a \link[base]{numeric} scalar of the degree of freedom, 
-#' with a \link[base]{numeric} scalar attribute `'stderr'` of the standard error of the mean-difference.
+#' Function [Gosset_Welch] returns a \link[base]{numeric} scalar or \link[base]{vector} 
+#' of the degree of freedom, with \link[base]{attributes},
+#' \describe{
+#' \item{`attr(., 'stderr')`}{\link[base]{numeric}, standard error \eqn{s_{\bar\Delta}}}
+#' \item{`attr(., 'stderr2')`}{\link[base]{numeric}, \eqn{s^2_{\bar\Delta}}, variance of \eqn{\bar\Delta}}
+#' } 
 #' 
 #' @references 
 #' Student's \eqn{t}-test by William Sealy Gosset, \doi{10.1093/biomet/6.1.1}.
@@ -42,9 +53,6 @@
 #' 
 # \url{https://en.wikipedia.org/wiki/Student%27s_t-test}
 #' 
-#' @seealso 
-#' \link[stats]{t.test}
-#' 
 #' @examples 
 #' x = rnorm(32L, sd = 1.6); y = rnorm(57L, sd = 2.1)
 #' vx = var(x); vy = var(y); nx = length(x); ny = length(y)
@@ -52,7 +60,6 @@
 #' Gosset_Welch(v1 = vx, v0 = vy, n1 = nx, n0 = ny, var.equal = FALSE)
 #' t.test(x, y, var.equal = TRUE)[c('parameter', 'stderr')]
 #' Gosset_Welch(v1 = vx, v0 = vy, n1 = nx, n0 = ny, var.equal = TRUE)
-#' 
 #' @keywords internal
 #' @export
 Gosset_Welch <- function(s1, s0, v1 = s1^2, v0 = s0^2, n1, n0, var.equal = FALSE) {
@@ -70,6 +77,7 @@ Gosset_Welch <- function(s1, s0, v1 = s1^2, v0 = s0^2, n1, n0, var.equal = FALSE
     ret <- vd^2 / (.v1^2/(n1-1L) + .v0^2/(n0-1L))
   }
   attr(ret, which = 'stderr') <- sqrt(vd)
+  attr(ret, which = 'stderr2') <- vd
   return(ret)
 }
 
