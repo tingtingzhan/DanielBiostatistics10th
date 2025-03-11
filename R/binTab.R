33267ef9db0ce366e93753b62d9d2e7a6ce55177
Tingting Zhan <tingtingzhan@gmail.com>
2025-03-11 17:16:35 -0400

    test

--- R/binTab.R
+++ R/binTab.R
@@ -275,3 +275,33 @@ npv <- function(prevalence, sensitivity, specificity) {
 }
 
 
+
+
+
+
+
+
+#' @title Description for \link[DanielBiostatistics10th]{binTab} Object
+#' 
+#' @description
+#' description for \link[DanielBiostatistics10th]{binTab} object.
+#' 
+#' @param x a \link[DanielBiostatistics10th]{binTab}
+#' 
+#' @returns 
+#' Function [Sprintf.binTab()] returns a \link[base]{character} scalar.
+#' 
+#' @keywords internal
+#' @export
+Sprintf.binTab <- function(x) {
+  dnm <- dimnames(x)
+  sprintf(fmt = 'Sensitivity, specificity and predictive values, as well as their 95%% exact confidence intervals, are provided for the 2-by-2 table of `%s` and `%s`.',
+          names(dnm)[1L], names(dnm)[2L])
+}
+
+
+
+
+
+
+
